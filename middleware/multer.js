c64b01c401c644c2ea615329a457f3f1b1983226
radiancegeorge <radiancegeorge@outlook.com>
2021-10-23 23:50:33 +0100

    updates

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,7 +3,7 @@ const path = require("path");
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../uploads/"),
   filename: (req, file, cb) => {
-    cb(null, `${file.originalname}`);
+    cb(null, `blog_${Date.now()}_${file.originalname}`);
   },
 });
 const uploads = multer({
